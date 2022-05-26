0eca21cafc114ae82ebda94d5d376c37e5349c44
MAHMOOD UL ISLAM <mahmood.islam@gmail.com>
2022-05-26 23:44:32 +0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const incrementCount = () => {
+        // Update state with incremented value
+        setCount(count + 1);
+    }
+    return (
+        <div id="mainArea">
+            <p>button count: <span>{count}</span></p>
+            <button id="mainButton" onClick={incrementCount}>Increase</button>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('root')
+);
